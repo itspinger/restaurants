beb4c03cf6fa870a8d6b752b2d11d47f1bd72ae0
avelickovic <avelickovic8823rn@raf.rs>
2025-01-05 13:09:09 +0100

    added getaway

---
+++ api-getaway/src/test/java/raf/rs/apigetaway/ApiGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package raf.rs.apigetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGetawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
